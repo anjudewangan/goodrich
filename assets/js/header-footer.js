fa2b1317a04ac48eb98ac430472df1c3732af6f0
Your Name <you@example.com>
2024-01-13 17:26:25 +0530

    update

--- assets/js/header-footer.js
+++ assets/js/header-footer.js
@@ -1,17 +1,17 @@
 document.getElementById(
     "section-header"
-).innerHTML = ` <div class="navbar navbar-default mega-menu-fullwidth navbar-fixed-top" role="navigation">
-<div class="navbar navbar-main">
+).innerHTML = `   <div class="navbar navbar-default mega-menu-fullwidth navbar-fixed-top" role="navigation">
+<div class="navbar navbar-main navbar-middle">
     <div class="container">
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle">
-                <i class="fa fa-bars"></i>
-            </button>
             <a class="navbar-brand" href="index.html">
                 <img class="logo-default scroll-hide" src="./assets/images/logos/logo.png" alt="logo" />
                 <img class="logo-default scroll-show" src="./assets/images/logos/logo.png" alt="logo" />
                 <img class="logo-retina" src="./assets/images/logos/logo.png" alt="logo" />
             </a>
+            <button type="button" class="navbar-toggle">
+                <i class="fa fa-bars"></i>
+            </button>
         </div>
         <div class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
@@ -25,15 +25,17 @@ document.getElementById(
                 <li class="dropdown">
                     <a href="#" role="button">Blogs <span class="caret"></span></a>
                 </li>
+                <li class="logo-item">
+                    <a href="#">
+                        <img src="./assets/images/logos/logo.png" alt="logo">
+                    </a>
+                </li>
                 <li class="dropdown">
                     <a href="#" role="button">Media <span class="caret"></span></a>
                 </li>
                 <li class="dropdown mega-dropdown mega-tabs">
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#">Manufacturing Excellence
                         <span class="caret"></span></a>
-                </li>
-                <li class="dropdown mega-dropdown mega-tabs">
-                    <a href="#">Careers <span class="caret"></span></a>
                 </li>
                 <li class="dropdown mega-dropdown mega-tabs">
                     <a href="#">Get in Touch <span class="caret"></span></a>
@@ -44,7 +46,7 @@ document.getElementById(
 </div>
 </div>`;
 
-document.getElementById("section-footer").innerHTML = ` <div class="content">
+document.getElementById("section-footer").innerHTML = `     <div class="content">
 <div class="container">
     <div class="row">
         <div class="col-md-3 footer-left">
@@ -57,45 +59,69 @@ document.getElementById("section-footer").innerHTML = ` <div class="content">
                 <a target="_blank" href="#" data-social="share-instgram"><i class="fa fa-instagram"></i></a>
                 <a target="_blank" href="#" data-social="share-linkedin"><i class="fa fa-linkedin"></i></a>
             </div>
-            <p><b></b></p>
+            <p><a href="#">Carrers</a></p>
         </div>
         <div class="col-md-3 footer-right text-left">
             <h5 class="text-bold">USEFUL LINKS</h5>
             <hr class="space s" />
+            <p><a href="#">Dehydrated Potato Products</a></p>
             <p><a href="#">Potato Flakes</a></p>
-            <p><a href="#">Potato Granules</a></p>
-            <p><a href="#">Dehydrated Potatoes</a></p>
-            <p><a href="#">Potato Starch</a></p>
             <p><a href="#">Potato Flour</a></p>
-            <p><a href="#">Potato Pellets</a></p>
+            <p><a href="#">Potato Granules</a></p>
+            <p><a href="#">Potato Air-dried Pieces</a></p>
         </div>
         <div class="col-md-3 footer-right text-left">
             <h5 class="text-bold">CONTACT DETAILS</h5>
             <hr class="space s" />
             <p><i class="fa fa-map-marker" aria-hidden="true"></i> GOODRICH CEREALS</p>
-            <p>Opposite Hotel Deventure, 1st Floor, Bank of Baroda Building, Namastey Chowk, Karnal - 132001, Haryana (India)</p>
+            <p>Opposite Hotel Deventure, 1st Floor, Bank of Baroda Building, Namastey Chowk, Karnal -
+                132001, Haryana (India)</p>
             <hr>
-            <p><i class="fa fa-envelope" aria-hidden="true"></i> <a href="info@goodrichcereals.com">info@goodrichcereals.com</a></p>
+            <p><i class="fa fa-envelope" aria-hidden="true"></i> <a
+                    href="info@goodrichcereals.com">info@goodrichcereals.com</a></p>
             <hr>
             <p><i class="fa fa-mobile" aria-hidden="true"></i> <a href=""> +91-9817700710</a></p>
             <hr>
             <p><i class="fa fa-whatsapp" aria-hidden="true"></i> <a href=""> +91 9817700710</a></p>
         </div>
         <div class="col-md-3 footer-right text-left">
-            <h5 class="text-bold">INTERNATIONAL OFFICES</h5>
+            <h5 class="text-bold">OUR OFFICES</h5>
             <hr class="space s" />
+            <p><i class="fa fa-map-marker" aria-hidden="true"></i> LONDON</p>
+            <p><i class="fa fa-envelope" aria-hidden="true"></i> <a
+                    href="hb.intl@goodrichcereals.in">hb.intl@goodrichcereals.in</a></p>
+            <hr>
+            <p><i class="fa fa-map-marker" aria-hidden="true"></i> VIETNAM</p>
+            <p><i class="fa fa-envelope" aria-hidden="true"></i> <a
+                    href="veitnam@goodrichcereals.in">veitnam@goodrichcereals.in</a></p>
+            <hr>
+            <p><i class="fa fa-map-marker" aria-hidden="true"></i> LATAM</p>
+            <p><i class="fa fa-envelope" aria-hidden="true"></i> <a
+                    href="latam@goodrichcereals.in">latam@goodrichcereals.in</a></p>
+            <hr>
             <p><i class="fa fa-map-marker" aria-hidden="true"></i> MELBOURNE</p>
             <hr>
-            <p><a href="#">Copyright</a></p>
-            <p><a href="#">Privacy Policy</a></p>
-            <p><a href="#">Terms & Condition</a></p>
+            <p><i class="fa fa-map-marker" aria-hidden="true"></i> AHEMDABAD</p>
         </div>
     </div>
 </div>
 <div class="row copy-row">
-    <div class="col-md-12 copy-text">
-        © 2024 - <a
-            href="https://www.patiodigital.com/" target="_blank">Patio Digital Pvt. Ltd.</a>
+    <div class="col-md-6 text-center">
+        © 2024 - <a href="https://www.patiodigital.com/" target="_blank">Patio Digital Pvt. Ltd.</a>
+    </div>
+    <div class="col-md-6">
+        <div class="row">
+            <div class="col-md-4">
+                <p><a href="#">Terms & Condition</a></p>
+            </div>
+            <div class="col-md-4">
+                <p><a href="#">Privacy Policy</a></p>
+            </div>
+            <div class="col-md-4">
+                <p><a href="#">Site Map</a></p>
+            </div>
+        </div>
     </div>
 </div>
-</div> `;
+</div>
+`;
