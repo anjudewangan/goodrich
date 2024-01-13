2e0c36bf63e0ba986af3e7259aa110702866f3a5
Your Name <you@example.com>
2024-01-13 13:48:32 +0530

    initial commit

---
+++ assets/js/header-footer.js
@@ -0,0 +1,101 @@
+document.getElementById(
+    "section-header"
+).innerHTML = ` <div class="navbar navbar-default mega-menu-fullwidth navbar-fixed-top" role="navigation">
+<div class="navbar navbar-main">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle">
+                <i class="fa fa-bars"></i>
+            </button>
+            <a class="navbar-brand" href="index.html">
+                <img class="logo-default scroll-hide" src="./assets/images/logos/logo.png" alt="logo" />
+                <img class="logo-default scroll-show" src="./assets/images/logos/logo.png" alt="logo" />
+                <img class="logo-retina" src="./assets/images/logos/logo.png" alt="logo" />
+            </a>
+        </div>
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="dropdown active">
+                    <a href="index.html">Our Story <span class="caret"></span></a>
+                </li>
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">Dehydrated Potato
+                        Products <span class="caret"></span></a>
+                </li>
+                <li class="dropdown">
+                    <a href="#" role="button">Blogs <span class="caret"></span></a>
+                </li>
+                <li class="dropdown">
+                    <a href="#" role="button">Media <span class="caret"></span></a>
+                </li>
+                <li class="dropdown mega-dropdown mega-tabs">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">Manufacturing Excellence
+                        <span class="caret"></span></a>
+                </li>
+                <li class="dropdown mega-dropdown mega-tabs">
+                    <a href="#">Careers <span class="caret"></span></a>
+                </li>
+                <li class="dropdown mega-dropdown mega-tabs">
+                    <a href="#">Get in Touch <span class="caret"></span></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+</div>`;
+
+document.getElementById("section-footer").innerHTML = ` <div class="content">
+<div class="container">
+    <div class="row">
+        <div class="col-md-3 footer-left">
+            <img src="./assets/images/logos/logo.png" alt="" class="footer-logo" />
+            <hr class="space s" />
+            <p>Bulk Supplies of All Dehydrated Potato Products</p>
+            <div class="btn-group social-group btn-group-icons">
+                <a target="_blank" href="#" data-social="share-facebook"><i class="fa fa-facebook"></i></a>
+                <a target="_blank" href="#" data-social="share-youtube"><i class="fa fa-youtube"></i></a>
+                <a target="_blank" href="#" data-social="share-instgram"><i class="fa fa-instagram"></i></a>
+                <a target="_blank" href="#" data-social="share-linkedin"><i class="fa fa-linkedin"></i></a>
+            </div>
+            <p><b></b></p>
+        </div>
+        <div class="col-md-3 footer-right text-left">
+            <h5 class="text-bold">USEFUL LINKS</h5>
+            <hr class="space s" />
+            <p><a href="#">Potato Flakes</a></p>
+            <p><a href="#">Potato Granules</a></p>
+            <p><a href="#">Dehydrated Potatoes</a></p>
+            <p><a href="#">Potato Starch</a></p>
+            <p><a href="#">Potato Flour</a></p>
+            <p><a href="#">Potato Pellets</a></p>
+        </div>
+        <div class="col-md-3 footer-right text-left">
+            <h5 class="text-bold">CONTACT DETAILS</h5>
+            <hr class="space s" />
+            <p><i class="fa fa-map-marker" aria-hidden="true"></i> GOODRICH CEREALS</p>
+            <p>Opposite Hotel Deventure, 1st Floor, Bank of Baroda Building, Namastey Chowk, Karnal - 132001, Haryana (India)</p>
+            <hr>
+            <p><i class="fa fa-envelope" aria-hidden="true"></i> <a href="info@goodrichcereals.com">info@goodrichcereals.com</a></p>
+            <hr>
+            <p><i class="fa fa-mobile" aria-hidden="true"></i> <a href=""> +91-9817700710</a></p>
+            <hr>
+            <p><i class="fa fa-whatsapp" aria-hidden="true"></i> <a href=""> +91 9817700710</a></p>
+        </div>
+        <div class="col-md-3 footer-right text-left">
+            <h5 class="text-bold">INTERNATIONAL OFFICES</h5>
+            <hr class="space s" />
+            <p><i class="fa fa-map-marker" aria-hidden="true"></i> MELBOURNE</p>
+            <hr>
+            <p><a href="#">Copyright</a></p>
+            <p><a href="#">Privacy Policy</a></p>
+            <p><a href="#">Terms & Condition</a></p>
+        </div>
+    </div>
+</div>
+<div class="row copy-row">
+    <div class="col-md-12 copy-text">
+        © 2024 - <a
+            href="https://www.patiodigital.com/" target="_blank">Patio Digital Pvt. Ltd.</a>
+    </div>
+</div>
+</div> `;
